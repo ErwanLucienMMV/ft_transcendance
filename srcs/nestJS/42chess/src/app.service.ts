d50561ea6e097a2511ab0a1c03ed29709f16914c
ErwanLucienMMV <ultimemmv@gmail.com>
2026-09-22 16:38:26 +0200

    removed useless strings so we can PR more easily I think

--- srcs/nestJS/42chess/src/app.service.ts
+++ srcs/nestJS/42chess/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! We are ready to code, and passing the live dev test';
+    return 'Hello World!';
   }
 }
